46543abe77ff9264d089f0ed573d542b6d8f2717
Árpád Tamási <arpad.tamasi@rollbar.com>
2021-03-14 09:00:47 +0100

    display brightness on button click

--- main.ts
+++ main.ts
@@ -1,36 +1,55 @@
-let brightnessSuppress = 1000
+let brightnessSetting = 3
+let compassEnabled = true
 
-function getCosineSimilarity(deg1: number, deg2: number): number {
+function getCosineSimilarity(deg1: number, deg2: number) {
     return Math.cos((deg1 - deg2) * Math.PI / 180)
 }
 
 input.onButtonPressed(Button.A, function () {
-    if (brightnessSuppress < 10000)
-        brightnessSuppress *= 10
+    if (brightnessSetting > 1) {
+        brightnessSetting = brightnessSetting - 1
+    }
+    showBrightness()
 })
 
 input.onButtonPressed(Button.B, function () {
-    if (brightnessSuppress > 1)
-        brightnessSuppress /= 10
+    if (brightnessSetting < 5) {
+        brightnessSetting = brightnessSetting + 1
+    }
+    showBrightness()
 })
 
-function getPixelAngle(x: number, y: number): number {
+function showBrightness() {
+    compassEnabled = false
+    basic.clearScreen()
+    led.plotBarGraph(brightnessSetting, 5)
+    basic.pause(1000)
+    compassEnabled = true
+}
+
+function getBrightness(similarity: number) {
+    return Math.exp((similarity - 1) * Math.exp(5 - brightnessSetting))
+}
+
+function getPixelAngle(x: number, y: number) {
     return 90 + Math.round(Math.atan2(y - 2, x - 2) * 180 / Math.PI)
 }
 
-basic.forever(function on_forever() {
-    let brightness: number;
 
-    for (let x = 0; x < 5; x++) {
-        for (let y = 0; y < 5; y++) {
-            let compassHeading = - input.compassHeading()
-            let pixelHeading = getPixelAngle(x, y)
-            let headingSimilarity = getCosineSimilarity(compassHeading, pixelHeading)
+function showCompass() {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
             if (x == 2 && y == 2) {
-                headingSimilarity = 1
+                led.plot(x, y)
+            } else {
+                led.plotBrightness(x, y, 255 * getBrightness(getCosineSimilarity(0 - input.compassHeading(), getPixelAngle(x, y))))
             }
-            let brightness = Math.exp((headingSimilarity - 1) * brightnessSuppress)
-            led.plotBrightness(x, y, 255 * brightness)
         }
+    }
+}
+
+basic.forever(function () {
+    if (compassEnabled) {
+        showCompass();
     }
 })
